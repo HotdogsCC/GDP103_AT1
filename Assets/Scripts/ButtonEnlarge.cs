d20885bb6f9231d95c21646c41d28b4e091b8f88
Charlie Campbell <chacha9669@hotmail.com>
2024-08-13 19:59:28 +1000

    added responive buttons and such

---
+++ Assets/Scripts/ButtonEnlarge.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEnlarge : MonoBehaviour
+{
+    [SerializeField] private float normalX = 393.5f;
+    [SerializeField] private float normalY = 125.1f;
+    private RectTransform _transform;
+    private bool pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<RectTransform>();
+    }
+    private void Update()
+    {
+        if (pressed)
+        {
+            _transform.sizeDelta = Vector2.Lerp(_transform.sizeDelta, new Vector2(normalX + 50, normalY + 20), 5 * Time.deltaTime);
+        }
+        else
+        {
+            _transform.sizeDelta = Vector2.Lerp(_transform.sizeDelta, new Vector2(normalX, normalY), 5 * Time.deltaTime);
+        }
+    }
+
+    public void Enlarge()
+    {
+        pressed = true;
+    }
+
+    public void Shrink()
+    {
+        pressed = false;
+    }
+}
