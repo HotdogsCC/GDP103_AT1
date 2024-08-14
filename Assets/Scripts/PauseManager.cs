f288d8df66e41935eb7931f8419362cecd2fff47
Charlie Campbell <chacha9669@hotmail.com>
2024-08-14 23:03:34 +1000

    terminal minigame and pause screen

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,57 @@
+using Cinemachine;
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool gamePaused = false;
+    [SerializeField] private ThirdPersonController player;
+    [SerializeField] private Transform targetPos;
+
+    [SerializeField] private GameObject resumeButton;
+    [SerializeField] private GameObject quitButton;
+
+    private void Update()
+    {
+        if (!gamePaused)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) && FindObjectOfType<TerminalGame>().gameStarted == false)
+            {
+                player.enabled = false;
+                Camera.main.GetComponent<CinemachineBrain>().enabled = false;
+                gamePaused = true;
+                resumeButton.SetActive(true);
+                quitButton.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+        else
+        {
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, targetPos.position, Time.deltaTime * 5);
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, targetPos.rotation, Time.deltaTime * 5);
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        player.enabled = true;
+        Camera.main.GetComponent<CinemachineBrain>().enabled = true;
+        gamePaused = false;
+        resumeButton.SetActive(false);
+        quitButton.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/TerminalGame.cs
@@ -0,0 +1,93 @@
+using Cinemachine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TerminalGame : MonoBehaviour
+{
+    [SerializeField] Transform cameraPos;
+    [SerializeField] AudioClip scaryMusic;
+    [SerializeField] AudioSource jukebox;
+    [SerializeField] GameObject particles;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject clearSound;
+    public bool gameStarted = false;
+    private bool math = false;
+
+    private int num1;
+    private int num2;
+
+    private string question;
+    private string answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 = UnityEngine.Random.Range(0, 100);
+        num2 = UnityEngine.Random.Range(0, 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (math)
+        {
+            answer += Input.inputString;
+
+            if (answer.Contains("\b"))
+            {
+                answer = null;
+                Instantiate(clearSound);
+            }
+
+            if(Int32.TryParse(answer, out int num))
+            {
+                if(num == num1 + num2)
+                {
+                    Debug.Log("game win");
+                    math = false;
+                    gameStarted = false;
+                    text.text = "You win!";
+                    StartCoroutine(WaitAMo());
+                    return;
+                }
+            }
+            text.text = question + "\n\n" + answer;
+        }
+
+        if(gameStarted)
+        {
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, cameraPos.position, Time.deltaTime * 2);
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, cameraPos.rotation, Time.deltaTime * 2);
+
+            if(Input.GetKeyDown(KeyCode.Return) && !math)
+            {
+                jukebox.clip = scaryMusic;
+                jukebox.Play();
+                Instantiate(particles, new Vector3(4.59f, 6.410817f, 9.09f), Quaternion.identity);
+                math = true;
+                question = "Answer the math question!! \n\n " + num1 + " + " + num2 + "\n\n";
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            Camera.main.GetComponent<CinemachineBrain>().enabled = false;
+            gameStarted = true;
+            FindFirstObjectByType<CharacterController>().gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator WaitAMo()
+    {
+        yield return new WaitForSeconds(4);
+        Cursor.lockState = CursorLockMode.None;
+        SceneManager.LoadSceneAsync(0);
+    }
+}
